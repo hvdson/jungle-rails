8c23ab179f0bcfc27b163f6f37d0570c7e9d0142
hvdson <hvdsonmusic@gmail.com>
2018-03-01 02:36:46 +0000

    finished user authentication - had to rollback table b/c only had name as a column - first name and last name is required

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,24 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # if user exists and password correct
+    if user && user.authenticate(params[:password])
+      # Save user id inside cookie
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # send back to login form
+      redirect_to 'login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   get '/signup' => 'users#new'
-  get '/login' => 'users#create'
+  post '/users' => 'users#create'
   
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
