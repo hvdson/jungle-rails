a3eb77365cfd590899bad1bf6e4ba095383ed97e
hvdson <hvdsonmusic@gmail.com>
2018-03-02 21:19:43 +0000

    Step 8. made form for review... i think it's working

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :user
-  belongs_to :product
+  belongs_to :product  
 
   validates :user_id, presence: true
   validates :product_id, presence: true
